1ff01ea612f62c910c03c554e1db35a61dae49c3
Alexandre J. Martins <alexandre@ajmartins.com.br>
2021-01-12 00:13:04 -0300

    Bug fixed on TextAreaField file

--- core/form/TextAreaField.php
+++ core/form/TextAreaField.php
@@ -1,11 +1,10 @@
- <?php
+<?php
 /**
  *	This framework has made following this tutorial:
  *	https://www.youtube.com/watch?v=6ERdu4k62wI
  *	
  */
 namespace app\core\form;
-
 /*
  *	@author Alexandre J. Martins <contato@ajmartins.com.br>
  *	@package app\core\form
